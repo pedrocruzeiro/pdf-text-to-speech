a35e6ecb5b1ac288b9078e10d3c06331b6a7984c
Cruzeiro Pedro (SO/OPM12-Lis) <pedro.cruzeiro@pt.bosch.com>
2023-01-03 12:39:14 +0000

    [NO JIRA][Frontend]-[Added valid download format mp3 file]

--- frontend/pdf-text-to-speech/src/components/DropFile.vue
+++ frontend/pdf-text-to-speech/src/components/DropFile.vue
@@ -40,26 +40,24 @@ import axios from 'axios'
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 }
+                , //
+                responseType: "blob",
+                config: {
+                    headers: {
+                    'Accept': 'audio/mpeg'
+                }
+    }
               }
-            ).then(function (response) {
+              , {responseType: 'blob'}).then(function (response) {
                 console.log(response)
-    let fileName = response.headers["content-disposition"].split("filename=")[1];
-    if (window.navigator && window.navigator.msSaveOrOpenBlob) { // IE variant
-        window.navigator.msSaveOrOpenBlob(new Blob([response.data],
-                { type: 'audio/mpeg' }
-            ),
-            fileName
-        );
-    } else {
         const url = window.URL.createObjectURL(new Blob([response.data],
-            { type: 'application/pdf' }));
+            { type: 'audio/mpeg' }));
         const link = document.createElement('a');
         link.href = url;
         link.setAttribute('download',
             response.headers["content-disposition"].split("filename=")[1]);
         document.body.appendChild(link);
         link.click();
-    }
     }
 )
         .catch(function(){
